b9fb3b5ef82dba132d7bd960817691ebfffad738
audukehinde <auduomohkehinde@gmail.com>
2021-11-24 06:38:56 -0500

    Complete working with javascript classes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 class Book {
-  constructor(title, author) {
+  constructor(title, author, id = Math.floor(Math.random() * 10000)) {
     this.title = title;
     this.author = author;
-    this.id = Math.floor(Math.random() * 10000);
+    this.id = id;
   }
 
   add() {
@@ -17,6 +17,14 @@ class Book {
         <hr>
       `;
     bookDiv.innerHTML += bookElement;
+    const inputData = {
+      title: this.title,
+      author: this.author,
+      id: this.id,
+    };
+    const storedData = JSON.parse(localStorage.getItem('storedData'));
+    storedData.push(inputData);
+    localStorage.setItem('storedData', JSON.stringify(storedData));
     bookDiv.querySelector('.removeBtn').addEventListener('click', () => {
       this.remove();
     });
@@ -26,13 +34,34 @@ class Book {
   remove() {
     const bookDiv = document.getElementById(`book-${this.id}`);
     bookDiv.parentElement.removeChild(bookDiv);
+    let storedData = JSON.parse(localStorage.getItem('storedData'));
+    storedData = storedData.filter((bookobj) => {
+      if (bookobj.id === this.id) {
+        return false;
+      }
+      return true;
+    });
+    localStorage.setItem('storedData', JSON.stringify(storedData));
   }
 }
 
-const addBtn = document.getElementById('addBtn');
-addBtn.addEventListener('click', () => {
-  const bookTitle = document.getElementById('title').value;
-  const bookAuthor = document.getElementById('author').value;
-  const book = new Book(bookTitle, bookAuthor);
-  book.add();
-});
+const initialLoad = () => {
+  const storedData = JSON.parse(localStorage.getItem('storedData')) || [];
+  if (storedData) {
+    storedData.forEach((book) => {
+      const newBook = new Book(book.title, book.author, book.id);
+      newBook.add();
+    });
+  }
+  localStorage.setItem('storedData', JSON.stringify(storedData));
+
+  const addBtn = document.getElementById('addBtn');
+  addBtn.addEventListener('click', () => {
+    const bookTitle = document.getElementById('title').value;
+    const bookAuthor = document.getElementById('author').value;
+    const book = new Book(bookTitle, bookAuthor);
+    book.add();
+  });
+};
+
+initialLoad();
